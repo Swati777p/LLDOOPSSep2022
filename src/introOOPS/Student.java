2dc6bac90e4170713c9799cfdf10df3bdda60435
Swati Patil <swati@Swatis-MacBook-Air.local>
2022-09-04 17:46:19 +0530

    Initial commit

---
+++ src/introOOPS/Student.java
@@ -0,0 +1,18 @@
+package introOOPS;
+
+public class Student {
+    String name;
+    String batchName;
+    int age;
+    double psp;
+
+    void changeBatch(String newBatch) {
+        batchName = newBatch;
+    }
+
+    void giveMockInterview() {
+        System.out.println("Giving mock interview");
+    }
+
+
+}
---
+++ src/introOOPS/main.java
@@ -0,0 +1,12 @@
+package introOOPS;
+
+public class main {
+    public static void Main(String[] args){
+        Student naman = new Student();
+        naman.name = "Naman";
+        naman.age = 24;
+
+        Student dinesh = new Student();
+        dinesh.name = "Dinesh";
+    }
+}
